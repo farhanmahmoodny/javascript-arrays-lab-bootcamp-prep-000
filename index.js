f1887425d24d303b61518588bf39265c5fc59601
Farhan Mahmood <farhanmahmoodny@gmail.com>
2018-12-18 23:51:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 function destructivelyAppendKitten(name){
   console.log(kittens.push(name))
 }
+
+function destructivelyPrependKitten(name){
+  console.log(kittens.unshitft(name))
+}
