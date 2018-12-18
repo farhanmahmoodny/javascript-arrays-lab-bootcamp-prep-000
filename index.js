ea0986a90f831b116bbf46083666e1ade49262f6
Farhan Mahmood <farhanmahmoodny@gmail.com>
2018-12-18 23:37:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(){
+  console.log(kittens.concat('Felix'))
+}
